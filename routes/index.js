6580552d351c96136b565090777c1a06e0d242d2
sam.sayadzad <sam.sayadzad@silentcircle-llc.com>
2014-09-07 04:56:33 -0700

    A (failed) test of protobufjs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var ProtoBuf = require("protobufjs");
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+
+  var builder = ProtoBuf.loadProtoFile("data/test.proto");
+
+  console.log(builder);
 });
 
 module.exports = router;
